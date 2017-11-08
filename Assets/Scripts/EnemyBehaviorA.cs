96297f0752e0b2050f5e0dd053322729f84afadc
Edward Opich <edwardOpich3@gmail.com>
2017-11-08 14:34:00 -0500

    Enemy Type A created
    -On spawn, enemy type A will fling itself in a gravity-defined arc to the left.

---
+++ Assets/Scripts/EnemyBehaviorA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviorA : MonoBehaviour
+{
+	private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		rigidbody.AddForce(new Vector2(-500.0f, 400.0f));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
