816d86e25a62d48252fb46e28b0b08e1fe638e4c
Nicole Griffin <nag5034@g.rit.edu>
2017-11-10 11:01:40 -0500

    Added Bullet and Shooting to player

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Translate(speed, 0, 0);
+
+        if(Camera.main.WorldToViewportPoint(transform.position).x > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
