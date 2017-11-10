ebef8de7bf8b205a12e93c51a35c5ef07c13d893
Nicole Griffin <nag5034@g.rit.edu>
2017-11-09 22:57:47 -0500

    Added Enemy B and Behavior Script

---
+++ Assets/Scripts/EnemyBehaviorB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviorB : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private Vector3 pos;
+    public float speed;
+    public float frequency;
+    public float magnitude;
+    private Vector3 axis;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        pos = transform.position;
+        axis = transform.right;
+        //rigidbody = GetComponent<Rigidbody2D>();
+        //rigidbody.AddForce(new Vector2(-500.0f, 400.0f));
+    }
+
+// Update is called once per frame
+void Update ()
+    {
+        pos += transform.up * Time.deltaTime * speed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
