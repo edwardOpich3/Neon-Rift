2a2fdaf2cb9613ced550cfc18bdb6481dcfd1342
Edward Opich <edwardOpich3@gmail.com>
2017-11-10 00:03:23 -0500

    Nicholas' player movement script added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		if(Input.GetKey(KeyCode.W))
+        {
+			transform.Translate(new Vector3(0.0f, speed, 0.0f));
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+			transform.Translate(new Vector3(0.0f, -speed, 0.0f));
+        }
+	}
+}
