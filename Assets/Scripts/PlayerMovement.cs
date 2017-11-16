17b095dc052adf7f1699525216afb2be0888af71
Edward Opich <edwardOpich3@gmail.com>
2017-11-16 01:54:46 -0500

    Implemented Invincibility frames
    -After being hit, the player now has temporary invincibility to avoid cheap HP depletion

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 0.1f;
+	public float invincibleTime = 2.0f;		// After being hit, time the player would have invincibility for, in seconds
 
 	private Spawning spawning;
+	private float invincibilityTimer;		// Amount of time player has had invincibility.
+	private bool isInvincible;				// Is the player currently invincible?
 
 	// Use this for initialization
 	void Start ()
 	{
 		spawning = GameObject.Find("Game Manager").GetComponent<Spawning>();
+		invincibilityTimer = 0.0f;
+		isInvincible = false;
 	}
 	
 	// Update is called once per frame
@@ -35,17 +40,28 @@ public class PlayerMovement : MonoBehaviour {
 				transform.Translate(0.0f, speed, 0.0f);
 			}
         }
+
+		if(isInvincible)
+		{
+			invincibilityTimer += Time.deltaTime;
+			if(invincibilityTimer >= invincibleTime)
+			{
+				isInvincible = false;
+				invincibilityTimer = 0.0f;
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if(other.gameObject.name.Substring(0, 5) == "Enemy")
+		if(other.gameObject.name.Substring(0, 5) == "Enemy" && !isInvincible)
 		{
 			// Replace this with whatever should happen when the player gets hit by an enemy!
 			if(!spawning.playerHit())
 			{
 				Destroy(gameObject);
 			}
+			isInvincible = true;
 		}
 	}
 }
