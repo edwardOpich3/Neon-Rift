53af767471f5e58da3b6af87becfb9e6a0ad0f1e
MAIN\eeo4537 <edwardOpich3@gmail.com>
2017-12-11 11:06:17 -0500

    Laser Light Show and Fixed meters

--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -40,8 +40,8 @@ public class Spawning : MonoBehaviour
 		currentTick = 0.0f;
 		score = 0;
 
-		rockOnMeter = 0.0f;
-		rockGodMeter = 0.0f;
+		rockOnMeter = 1.0f;
+		rockGodMeter = 0.5f;
 
         livesText.GetComponent<TextMesh>().text = "Lives: " + health;
         scoreText.GetComponent<TextMesh>().text = "Score: " + score;
@@ -76,10 +76,6 @@ public class Spawning : MonoBehaviour
 		{
 			rockOnMeter -= 0.2f * Time.deltaTime;
 
-			Vector3 barScale = progress.transform.localScale;
-			barScale -= new Vector3 (0.6f * Time.deltaTime, 0.0f, 0.0f);
-			progress.transform.localScale = barScale;
-
 			if(rockOnMeter <= 0.0f)
 			{
 				rockOnMeter = 0.0f;
@@ -92,11 +88,6 @@ public class Spawning : MonoBehaviour
 		else if(rockOnMeter == 1.0f)
 		{
 			rockGodMeter += 0.01f * Time.deltaTime;
-
-			// increasing size of progress bar
-			Vector3 rgBarScale = rgProgress.transform.localScale;
-			rgBarScale += new Vector3 (0.075f * Time.deltaTime, 0.0f, 0.0f);
-			rgProgress.transform.localScale = rgBarScale;
 
 			if(rockGodMeter >= 1.0f)
 			{
@@ -116,6 +107,9 @@ public class Spawning : MonoBehaviour
 
 			rockGodText.GetComponent<TextMesh>().text = "Rock GOD: " + rockGodMeter.ToString("P1");
 		}
+
+		progress.transform.localScale = new Vector3(rockOnMeter * 3.0f, 1.0f, 1.0f);
+		rgProgress.transform.localScale = new Vector3(rockGodMeter * 5.0f, 0.5f, 1.0f);
 	}
 
 	// Updates the currentEnemies count, as well as the score.
@@ -130,21 +124,11 @@ public class Spawning : MonoBehaviour
 		{
 			rockOnMeter += 0.05f;
 
-			// increasing size of progress bar
-			Vector3 barScale = progress.transform.localScale;
-			barScale += new Vector3 (0.15f, 0.0f, 0.0f);
-			progress.transform.localScale = barScale;
-
 			if(rockOnMeter >= 1.0f)
 			{
 				rockOnMeter = 1.0f;
 
 				rockGodMeter += 0.333f;
-
-				// increasing size of progress bar
-				Vector3 rgBarScale = rgProgress.transform.localScale;
-				rgBarScale += new Vector3 (1.0f, 0.0f, 0.0f);
-				rgProgress.transform.localScale = rgBarScale;
 
 				if(rockGodMeter >= 0.999f)
 				{
